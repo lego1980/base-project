6d7cc855ad27377568f9a2430beca02868325ee6
lego1980 <ronaldayson@hotmail.com>
2019-05-04 10:20:04 -0700

    more scss changes

--- src/components/views/parallax/ParallaxView.2.js
+++ src/components/views/parallax/ParallaxView.2.js
@@ -8,7 +8,8 @@ import { ROUTE_ACTIONS } from '../../../redux/actions/route/RoutesActions';
 
 // css
 import stylesView from '../../../styles/global/globalView.module.scss';
-import './ParallaxView.2.css';
+import stylesForms from '../../../styles/global/globalForm.module.scss';
+import styles from './ParallaxView.2.module.scss';
 
 export class ParallaxView extends React.Component {
   shouldComponentUpdate(nextProps, nextState) {
@@ -21,36 +22,36 @@ export class ParallaxView extends React.Component {
 
     return (
       <main>
-        <div className={"page parallax-view " + stylesView['view']}>
-          <div className={"section bgimg-1"}>
-            <div className={"box"}>
-              <div className={"inner-box"}>
+        <div className={"parallax-view " + stylesView['page'] + " " + stylesView['view']}>
+          <div className={styles['section'] + ' ' + styles['bgimg-1']}>
+            <div className={styles['box']}>
+              <div className={styles['inner-box']}>
                 <h1>Parallax Demo</h1>
                 <p>Parallax scrolling is a web site trend where the background content is moved at a different speed than the foreground content while scrolling. Nascetur per nec posuere turpis, lectus nec libero turpis nunc at, sed posuere mollis ullamcorper libero ante lectus, blandit pellentesque a, magna turpis est sapien duis blandit dignissim. Viverra interdum mi magna mi, morbi sociis.</p>
               </div>
             </div> 
           </div>          
 
-          <div className={"show-box"}>
-            <div className={"caption"}>
-              <span className={"border"}>
-                <Link to="/login/" className="link-button">LOG IN</Link>
+          <div className={styles['show-box']}>
+            <div className={styles['caption']}>
+              <span className={styles['border']}>
+                <Link to="/login/" className={stylesForms['link-button']}>LOG IN</Link>
               </span>
             </div>
           </div>
           
-          <div  className={"section bgimg-2"}>
-            <div className={"box"}>
-              <div className={"inner-box"}>
+          <div className={styles['section'] + ' ' + styles['bgimg-2']}>
+            <div className={styles['box']}>
+              <div className={styles['inner-box']}>
                 <h2>Parallax Demo</h2>
                 <p>Parallax scrolling is a web site trend where the background content is moved at a different speed than the foreground content while scrolling.</p>
               </div>
             </div> 
           </div>
 
-          <div className={"section bgimg-3"}>
-            <div className={"box"}>
-              <div className={"inner-box"}>              
+          <div className={styles['section'] + ' ' + styles['bgimg-3']}>
+            <div className={styles['box']}>
+              <div className={styles['inner-box']}>              
                 <p>Parallax scrolling is a web site trend where the background content is moved at a different speed than the foreground content while scrolling.</p>
               </div>
             </div> 
