92fe27b8914d22ca8815b649273c15d692953570
lego1980 <ronaldayson@hotmail.com>
2019-05-06 00:03:09 -0700

    added validation for required and pattern

--- src/components/views/login/LoginView.js
+++ src/components/views/login/LoginView.js
@@ -7,69 +7,110 @@ import { connect } from "react-redux";
 import { ROUTE_ACTIONS } from '../../../redux/actions/route/RoutesActions';
 
 //
-import { requiredField } from '../../../utils/FormValidations';
+import { validate } from '../../../utils/FormValidations';
 
 // css
 import stylesViews from '../../../styles/global/globalView.module.scss';
 import stylesForms from '../../../styles/global/globalForm.module.scss';
 import styles from './LogInView.module.scss';
 
-
-
 export class LogInView extends React.Component {
   
   constructor(props) {
     super(props);
     this.state = {
+      submitButton: false,
       username: "",
-      password: ""
+      password: "",
+      error: {
+        username: { 
+          valid : true,
+          msg: "" 
+        },
+        password: {
+          valid : true,
+          msg: ""  
+        }
+      }       
     };
   }
 
-  submitHandler = (e) => {
-    requiredField(e);
+  onSubmitHandler = (e) => {
+    let error = this.checkError();
+    if (error.length === 0) {
+      //proceed post
+      console.log("Posting...",error)
+    }
     e.preventDefault();
   }
 
-  onBlurHandler = (e) => {
-    let elm = e.target || null;
-    requiredField(elm).then(function(bool) {
-      console.log("bool",bool);
+  checkError = () => {
+    let that = this;  
+    let error = Object
+      .values(that.state.error)
+      .filter((item) => {
+        return item.valid === false;
+      });
+    return error;  
+  }
+
+  onChangeHandler = (e) => {   
+    let that = this;
+    let element = e.target || null;
+    that.setState({ 
+      [element["name"]] : element.value
+    }, () => {
+      if (element) {
+        validate(element).then((bool) => {
+            that.setState({ 
+                error: Object.assign(
+                    {}, 
+                    that.state.error, 
+                    { [element["name"]] : { valid : bool }}
+                )                      
+            }); 
+        }).then(() => {
+          if (that.state.username.length !== 0 && that.state.password.length !== 0 && that.checkError().length === 0) {
+            that.setState({ 
+              submitButton : true
+            });
+          } else {
+            that.setState({ 
+              submitButton : false
+            });
+          }
+        });   
+      }
     });
-    e.preventDefault();
   }
-
-  onChangeHandler = (e) => {
-    
-    // this.setState({
-    //   inputValue: e.target.value
-    // });
-  }
-
 
   render() {
+    // console.log("render",this.state.submitButton,this.state.username.length,this.state.password.length);
     return (
       <main className={stylesViews['page'] + " " + stylesViews['view'] + " " + styles['log-in-view']}>        
-        <form noValidate>
+        <form noValidate onSubmit={(e) => this.onSubmitHandler(e)}>
           <h1>User Login</h1>
           <input 
             type="text" 
             name="username" 
-            placeholder="username" 
-            onBlur={(e) => this.onBlurHandler(e)} 
+            placeholder="username"            
             onChange = {(e) => this.onChangeHandler(e)}
-            required 
+            className={(!this.state.error.username.valid) ? stylesForms['error']  : ''}
+            required
           />
           <input 
             type="password" 
             name="password" 
             placeholder="password"
-            onBlur={(e) => this.onBlurHandler(e)} 
             onChange = {(e) => this.onChangeHandler(e)}
-             
+            className={(!this.state.error.password.valid) ? stylesForms['error']  : ''}
+            required
           />
-          <input type="submit" value="LOG IN" onClick={(e) => this.onSubmitHandler(e)}/>
-          {/* <input type="reset" value="CLEAR" /> */}
+          <input 
+            type="submit" 
+            value="LOG IN"
+            disabled={!this.state.submitButton}
+          />     
           <Link to="/register/" className={stylesForms['link-button']}>REGISTER</Link>
         </form>
       </main>
--- src/utils/FormValidations.js
+++ src/utils/FormValidations.js
@@ -1,7 +1,30 @@
 export const requiredField = async(elm) => {
     let node = elm;
     if (node["required"] && node["value"].length === 0) {
-        return false
+        return false;
     }
     return true;
+}
+
+export const patternField = async(elm) => {
+    let node = elm;
+    if (node["pattern"] && node["value"].length !== 0) {
+        let pattern = node["pattern"];
+        let re = new RegExp(pattern);      
+        return re.test(node["value"]);
+    }
+    return true;
+}
+
+export const validate = async(element) => {
+    let elm = element;        
+    return requiredField(elm).then(function(bool) {
+        if (bool) {
+            return patternField(elm).then(function(bool2) {
+                return bool2;
+            });
+        } else {
+            return bool; 
+        }        
+    });
 }
